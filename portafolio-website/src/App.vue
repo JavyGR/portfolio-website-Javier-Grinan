4e5dddf77ae93d958539f6de54ece70ec44040c4
JavyGR <javigrinanr@gmail.com>
2024-10-10 02:04:07 -0500

    home layout

--- portafolio-website/src/App.vue
+++ portafolio-website/src/App.vue
@@ -1,11 +1,25 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link>
-    <router-link to="/about">About Me</router-link>
-    <router-link to="/#">Service</router-link>
-    <router-link to="/#">Projects</router-link>
-    <router-link to="/#">Blog</router-link>
-    <router-link to="/#">Contact</router-link>
+    <ul>
+      <li class="nav-item">
+        <router-link class="nav-link" to="/">Home</router-link>
+      </li>
+      <li class="nav-item">
+        <router-link class="nav-link" to="/about">About Me</router-link>
+      </li>
+      <li class="nav-item">
+        <router-link class="nav-link" to="/#">Service</router-link>
+      </li>
+      <li class="nav-item">
+        <router-link class="nav-link" to="/#">Projects</router-link>
+      </li>
+      <li class="nav-item">
+        <router-link class="nav-link" to="/#">Blog</router-link>
+      </li>
+      <li class="nav-item">
+        <router-link class="nav-link" to="/#">Contact</router-link>
+      </li>      
+    </ul>
   </nav>
   <router-view/>
 </template>
@@ -16,11 +30,39 @@
   padding: 0;
   box-sizing: border-box;
 }
+body {
+  background: linear-gradient(145deg, rgb(34, 52, 63), #0d1417);
+}
 #app {
   /* #5afecb */
   /* font family */
   height: 100vh;
   width: 100%;
-  background: linear-gradient(145deg, rgb(34, 52, 63), #0d1417);
+  text-decoration: none;
+  color: whitesmoke;
+  text-align: center;
+  display: flex;
+  flex-direction: column;
 }
+ul {
+  margin-bottom: 0;
+  padding: 25px;
+  height: 100%;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  list-style: none;
+}
+.nav-item {
+  padding: 0 30px;
+}
+.nav-link {
+  color: whitesmoke;
+  text-shadow: 0 0px 1px whitesmoke;
+}
+.active-nav-link {
+  text-decoration: underline;
+  text-shadow: 0 0px 3px whitesmoke;
+}
+
 </style>
---
+++ portafolio-website/src/components/HomeComponent.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="home">
+    <main>
+      <section>
+        
+      </section>
+    </main>
+    <aside>
+      <img src="@/assets/portrait.png" alt="Javier Grinan portrait">
+    </aside>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeComponent',
+  props: {
+    
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .home {
+    height: 100%;
+    display: grid;
+    grid-template-columns: 2.5fr 1fr;
+  }
+  main {
+    display: flex;
+    align-items: center;
+  }
+  section {
+    width: 100%;
+  }
+  aside {
+    display: flex;
+    align-items: center;
+    padding-right: 0px;
+  }
+  img {
+    height: auto;
+    width: 100%;
+    border-radius: 50px;
+    clip-path: inset(20px 60px 10px 0 round 50px)
+  }
+</style>
