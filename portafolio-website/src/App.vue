d4c6a798312581424f7b47761a24dad24357ca19
JavyGR <javigrinanr@gmail.com>
2024-10-10 01:01:00 -0500

    project without babel

--- portafolio-website/src/App.vue
+++ portafolio-website/src/App.vue
@@ -1,26 +1,26 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <nav>
+    <router-link to="/">Home</router-link>
+    <router-link to="/about">About Me</router-link>
+    <router-link to="/#">Service</router-link>
+    <router-link to="/#">Projects</router-link>
+    <router-link to="/#">Blog</router-link>
+    <router-link to="/#">Contact</router-link>
+  </nav>
+  <router-view/>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
 <style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  /* #5afecb */
+  /* font family */
+  height: 100vh;
+  width: 100%;
+  background: linear-gradient(145deg, rgb(34, 52, 63), #0d1417);
 }
 </style>
